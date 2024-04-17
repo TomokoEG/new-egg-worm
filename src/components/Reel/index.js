8ed1970741518fb62ac55decdc085d1737ad1e31
Viet Hung <viethung0823@gmail.com>
2024-04-17 09:42:31 +0700

    update indent jp text

--- src/components/Reel/index.js
+++ src/components/Reel/index.js
@@ -164,8 +164,8 @@ function Reel() {
 				<div className="gl-state-white"></div>
 				<div className="gl-statement-txt">
 					<p className="gl-state-intxt">
-						<span className="gl-state-line"></span>
-						<span>{t("reel.title")}</span>
+						<span className="gl-state-line pretitle">{t("reel.preTitle")}</span>
+						<span className="gl-state-line-trim">{t("reel.title")}</span>
 					</p>
 					<p className="gl-state-intxt">
 						<span className="gl-state-line"></span>
