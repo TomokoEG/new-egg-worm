2fb97d6a1057da7075cd7aa884ff7f767d87df98
Viet Hung <viethung0823@gmail.com>
2024-04-23 11:17:57 +0700

    update home-works-list

--- src/components/HomeWorks/index.js
+++ src/components/HomeWorks/index.js
@@ -33,24 +33,24 @@ function HomeWorks() {
 	const items = [
 		{
 			id: 1,
-			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/ac09a66e-3604-425e-a1ef-fe2def98bb74/288020857_10159108756540698_9000293709595520415_n.jpeg?format=2500w",
-			title: "BRUICHLADDICH",
+			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/00900994-9f04-4260-85f6-220debb6ed8c/434684871_920431673419453_8894548977249295541_n.jpg?format=2500w",
+			title: "TAIS",
 			description: t("WOKR.1"),
-			url: `https://www.eggworm.jp/${isJapanese ? 'jp' : 'en'}/our-works/bruichladdich`,
+			url: `https://www.eggworm.jp/${false ? "jp" : "en"}/our-works/tais`,
 		},
 		{
 			id: 2,
-			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1596417754268-0HYZGZ8BXFKG20TNHSEK/107051076_2987167684885523_3111168653457907190_n.jpg",
-			title: "TAXI OVATION",
+			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/b3b854fe-fd8c-4e59-9d81-5c06f7329778/Screenshot+2024-04-16+173505.png?format=2500w",
+			title: "ALEXANDRION GROUP",
 			description: t("WOKR.2"),
-			url: `https://www.eggworm.jp/${isJapanese ? 'jp' : 'en'}/our-works/taxi-ovation`,
+			url: `https://www.eggworm.jp/${false ? "jp" : "en"}/our-works/alexandrion-group`,
 		},
 		{
 			id: 3,
-			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/1596418713811-XAM7LLHGZWOES91AAL3D/28161740_10155415421745698_2751362336498237194_o_10155415421745698.jpg",
-			title: "ONITSUKA TIGER",
+			imgLink: "https://images.squarespace-cdn.com/content/v1/5eb624a9707ef42c472b42ec/499a2472-9885-4594-b1cc-3e65dc12a371/Untitled+%281%29+copy+2.png?format=2500w",
+			title: "NOTION",
 			description: t("WOKR.3"),
-			url: `https://www.eggworm.jp/${isJapanese ? 'jp' : 'en'}/our-works/onitsuka-tiger`,
+			url: `https://www.eggworm.jp/${false ? "jp" : "en"}/our-works/notion`,
 		},
 	];
 
