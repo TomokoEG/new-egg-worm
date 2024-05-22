ccef9a3894413be8a632d7891d7b4afd54289ca9
Viet Hung <viethung0823@gmail.com>
2024-05-22 21:44:18 +0700

    update script

--- src/scripts/replaceNewBuild.js
+++ src/scripts/replaceNewBuild.js
@@ -1,49 +1,47 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-const sourceDir = "./build/static/js";
+const sourceDirs = {
+	js: "./build/static/js",
+	css: "./build/static/css",
+};
 const targetDir = "./src/dict/pages/HomePage";
 const indexPath = path.join(targetDir, "index.html");
 
-fs.readdir(sourceDir, (err, sourceFiles) => {
-	if (err) throw err;
+async function replaceFiles(fileType) {
+	const sourceFiles = await fs.readdir(sourceDirs[fileType]);
 
-	const newFile = sourceFiles.find((file) => file.startsWith("main.") && file.endsWith(".js"));
+	const newFile = sourceFiles.find((file) => file.startsWith("main.") && file.endsWith(`.${fileType}`));
 	if (!newFile) {
-		console.log("No new JS file found");
+		console.log(`No new ${fileType.toUpperCase()} file found`);
 		return;
 	}
 
-	fs.readdir(targetDir, (err, targetFiles) => {
-		if (err) throw err;
+	const targetFiles = await fs.readdir(targetDir);
 
-		const oldFile = targetFiles.find((file) => file.startsWith("main.") && file.endsWith(".js"));
-		if (oldFile) {
-			fs.unlink(path.join(targetDir, oldFile), (err) => {
-				if (err) throw err;
-				console.log(`${oldFile} was deleted`);
-			});
-		}
+	const oldFile = targetFiles.find((file) => file.startsWith("main.") && file.endsWith(`.${fileType}`));
+	if (oldFile) {
+		await fs.unlink(path.join(targetDir, oldFile));
+		console.log(`${oldFile} was deleted`);
+	}
 
-		const sourceFile = path.join(sourceDir, newFile);
-		const targetFile = path.join(targetDir, newFile);
+	const sourceFile = path.join(sourceDirs[fileType], newFile);
+	const targetFile = path.join(targetDir, newFile);
 
-		fs.copyFile(sourceFile, targetFile, (err) => {
-			if (err) throw err;
-			console.log(`${newFile} was copied to ${targetDir}`);
+	await fs.copyFile(sourceFile, targetFile);
+	console.log(`${newFile} was copied to ${targetDir}`);
 
-			// After copying the file, replace the filename in index.html
-			fs.readFile(indexPath, "utf8", function (err, data) {
-				if (err) {
-					return console.log(err);
-				}
-				var result = data.replace(/main\.\w+\.min\.js/g, newFile.replace(".js", ".min.js"));
+	// After copying the file, replace the filename in index.html
+	const data = await fs.readFile(indexPath, "utf8");
+	var result = data.replace(new RegExp(`main\\.\\w+\\.min\\.${fileType}`, "g"), newFile.replace(`.${fileType}`, `.min.${fileType}`));
 
-				fs.writeFile(indexPath, result, "utf8", function (err) {
-					if (err) return console.log(err);
-					console.log(`Replaced old filename with ${newFile} in ${indexPath}`);
-				});
-			});
-		});
-	});
-});
+	await fs.writeFile(indexPath, result, "utf8");
+	console.log(`Replaced old filename with ${newFile} in ${indexPath}`);
+}
+
+async function main() {
+	await replaceFiles("js");
+	await replaceFiles("css");
+}
+
+main();
